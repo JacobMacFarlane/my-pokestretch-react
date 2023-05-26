1e60a71cf21c7a51ee7e737355adfe7915122c52
Adam Meza <Adammeza95@gmail.com>
2023-05-26 13:55:21 -0400

    Create app component. also empty main component

--- src/Component/App/App.tsx
+++ src/Component/App/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { Main } from '../Main/Main';
+
 import './App.css';
 
 function App() {
+  const [allCards, setCards] = useState([])
+  const [favorites, setFavorites] = useState([])
+
+
   return (
-   <h1>Foo</h1>
+    <main>
+      <h1>Foo</h1>
+      <Switch> 
+        <Route exact path='/' render={(props : any) => <Main data={allCards} /> }/>
+        <Route exact path='/favorites' render={(props : any) => <Main data={favorites} /> }/>
+
+      </Switch>
+    </main>
   );
 }
 
