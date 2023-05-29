bda234d076384ce27e9ed03036e7b64dad5af0ad
Sara Park <sspark106@gmail.com>
2023-05-29 15:51:06 -0600

    feat: Test that no pokemon cards exist on favorites url page

--- cypress/e2e/favorite_spec.cy.ts
+++ cypress/e2e/favorite_spec.cy.ts
@@ -5,5 +5,19 @@ describe('Favorites Page', () => {
         fixture: "pokeData.json"})
         .visit("http://localhost:3000/")
     })
-    
+
+    it('should go to a favorites url (page) when favorites link is clicked', () => {
+        cy.get('a')
+        .should('have.attr', 'href')
+        .then((href) => {
+          cy.visit("http://localhost:3000/favorites")
+        })
+
+        cy.url().should('include', '/favorites')
+    })
+      
+    it('should display no Pokemon cards when no favorite button is clicked', () => {
+        cy.visit("http://localhost:3000/favorites")
+        .get('.single-card-container').should('not.exist');
+    });
 })
