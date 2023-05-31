9fc160538e8c709dff8e3a7f5046a612c5c3bf6e
BriBourassa <111149043+BriBourassa@users.noreply.github.com>
2023-05-31 10:50:16 -0600

    Add background image, logos to Header

--- src/Component/Header/Header.tsx
+++ src/Component/Header/Header.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 import './Header.css'
 import { Link } from "react-router-dom"
+import pkmntitle from '../../assets/pkmn-deck-builder-2.png'
+import favs from '../../assets/favs-text.png'
 
 export const Header: React.FC = () => {
   return (
-    <header>
+    <header className="nav-header">
+
+    <div className="logo-and-favs-btn">
       <Link to="/">
-        <img src="" alt="Logo" />
+        <img src={pkmntitle} className="logo-img" alt="Logo" />
       </Link>
+
+        <Link to="/favorites">
+          <img src={favs} className="fav-img"/>
+        </Link>
+    </div>
+
       <nav className="navbar">
-        <Link to="/favorites">Favorites</Link>
-        <h1 className="banner">POKEMON CARDS!</h1>
+        <h1 className="banner">Welcome, Trainer!</h1>
         <h3>Browse cards and build your deck</h3>
       </nav>
+
     </header>
   )
 }
