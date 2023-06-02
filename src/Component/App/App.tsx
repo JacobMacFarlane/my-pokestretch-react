69c3b72d2a2e9c427bf39d61a52a2185d40f032f
BriBourassa <111149043+BriBourassa@users.noreply.github.com>
2023-06-02 17:30:56 -0600

    Refactor App and fix small TS details

--- src/Component/App/App.tsx
+++ src/Component/App/App.tsx
@@ -5,58 +5,81 @@ import { cleanData } from '../../utilites';
 import { Header } from '../Header/Header';
 import { ErrorComp } from '../Error/Error';
 import './App.css';
-import { PokemonCard } from '../Card/PokemonCard';
 import { fetchAllCards } from '../../apiCalls';
 
 export type PokeCard = {
-  image: string
-  name: string
-  types: string[]
-  cardId: string
-}
+  image: string;
+  name: string;
+  types: string[];
+  cardId: string;
+};
 
 const App: React.FC = () => {
-  const [allCards, setCards] = useState<PokeCard[]>([])
-  const [favorites, setFavorites] = useState<PokeCard[]>([])
-  const [error, setError] = useState<Error | null>(null)
-  const [loading, setLoading] = useState(true)
- 
-  const chooseCard = ( pokemon: PokeCard ) => {
-    const newFavState = [...favorites, pokemon]
-    setFavorites(newFavState)
-   }
+  const [allCards, setCards] = useState<PokeCard[]>([]);
+  const [favorites, setFavorites] = useState<PokeCard[]>([]);
+  const [error, setError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState(true);
 
-   const removeFavorite = (cardToRemove : PokeCard) => {
-    const updatedFavs = favorites.filter((card) => card.cardId !== cardToRemove.cardId)
-    setFavorites(updatedFavs)
-  }
+  const chooseCard = (pokemon: PokeCard) => {
+    const newFavState = [...favorites, pokemon];
+    setFavorites(newFavState);
+  };
 
-  useEffect(()=> {
+  const removeFavorite = (cardToRemove: PokeCard) => {
+    const updatedFavs = favorites.filter(
+      (card) => card.cardId !== cardToRemove.cardId
+    );
+    setFavorites(updatedFavs);
+  };
+
+  useEffect(() => {
     try {
-      fetchAllCards()
-        .then(data => {
-          const cardData = cleanData(data.data)
-          setCards(cardData)
-          setLoading(false)
-        })
-    } catch(err: any) {
-      // this guy's catching the throw!
-      setError(err)
+      fetchAllCards().then((data) => {
+        const cardData = cleanData(data.data);
+        setCards(cardData);
+        setLoading(false);
+      });
+    } catch (err: any) {
+      setError(err);
     }
-  }, [])
+  }, []);
 
   return (
-    <section className='main'>
+    <section className="main">
       <Header />
-      {loading && <div className='loading-text'>Loading...</div>}
-      {error && <div className='error-text'>{error.name}: {error.message}</div>}
-      <Switch> 
-        <Route exact path='/' render={(props : {}) => <Main data={ allCards } chooseCard={chooseCard} removeFavorite={removeFavorite}/> }/>
-        <Route exact path='/favorites' render={(props : {}) => <Main data={ favorites } chooseCard={chooseCard} removeFavorite={removeFavorite}/> }/>
+      {loading && <div className="loading-text">Loading...</div>}
+      {error && (
+        <div className="error-text">
+          {error.name}: {error.message}
+        </div>
+      )}
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <Main
+              data={allCards}
+              chooseCard={chooseCard}
+              removeFavorite={removeFavorite}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/favorites"
+          render={() => (
+            <Main
+              data={favorites}
+              chooseCard={chooseCard}
+              removeFavorite={removeFavorite}
+            />
+          )}
+        />
         <Route path="*" render={() => <ErrorComp />} />
       </Switch>
     </section>
   );
-}
+};
 
 export default App;
